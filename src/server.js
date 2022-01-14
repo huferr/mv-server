1c4c639396fb9e3ee41b92203a0985c632862bac
Hugo Ferreira <hugoferreirait@gmail.com>
2022-01-14 20:42:35 -0300

    chore: starting localhost

--- src/server.js
+++ src/server.js
@@ -1,1 +1,16 @@
-console.log('hello world')
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import { graphql } from "graphql";
+const app = express();
+const PORT = 3030;
+
+const schema = {};
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}))
+
+app.listen(PORT, () => {
+    console.log('hello world')
+})
