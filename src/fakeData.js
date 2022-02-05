04b01e3045373525d9e920838f9403f2775e521c
Hugo Ferreira <hugoferreirait@gmail.com>
2022-02-04 22:42:38 -0300

    feat: email field

--- src/fakeData.js
+++ src/fakeData.js
@@ -3,21 +3,25 @@ const UserList = [
       id: 1,
       nickname: 'hugold',
       mathscore: 1000,
+      email: 'hugo@hugo.com'
     }, 
     {
       id: 2,
       nickname: 'huferr',
       mathscore: 200,
+      email: 'huferr@gmail.com'
     },
     {
       id: 3,
       nickname: 'what',
       mathscore: 30,
+      email: 'what@gmail.com'
     },
     {
       id: 4,
       nickname: 'jao e o pe de taioba',
       mathscore: 500,
+      email: 'jao@gmail.com'
     }
 ]
 
