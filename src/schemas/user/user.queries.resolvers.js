5a6b46455d1194429be5bbc2303ded702b97646a
Hugo Ferreira <hugoferreirait@gmail.com>
2022-02-05 00:08:43 -0300

    chore: adding search user by nickname

--- src/schemas/user/user.queries.resolvers.js
+++ src/schemas/user/user.queries.resolvers.js
@@ -3,9 +3,18 @@ const { UserList } = require("../../fakeData")
 const users = () => UserList;
 
 const user = (parent, args) => {
+
+  if (args.id) {
     const id = Number(args.id);
     const user = UserList[id - 1];
-    return user;  
+    return user; 
+  } 
+
+  if (args.nickname) {
+      const nickname = String(args.nickname);
+      const userByNickname = UserList.find((u) => u.nickname === nickname);
+      return userByNickname;
+  }
 };
 
 const resolvers = {
