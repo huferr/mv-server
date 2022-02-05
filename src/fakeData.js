c75d2167bc698fbbc5c85c29763f16e51ebc314a
Hugo Ferreira <hugoferreirait@gmail.com>
2022-02-05 00:08:18 -0300

    chore: adding homework data

--- src/fakeData.js
+++ src/fakeData.js
@@ -3,25 +3,41 @@ const UserList = [
       id: 1,
       nickname: 'hugold',
       mathscore: 1000,
-      email: 'hugo@hugo.com'
+      email: 'hugo@hugo.com',
+      homeworks: {
+        done: 5,
+        undone: 195,
+      }
     }, 
     {
       id: 2,
       nickname: 'huferr',
       mathscore: 200,
-      email: 'huferr@gmail.com'
+      email: 'huferr@gmail.com',
+      homeworks: {
+        done: 10,
+        undone: 190,
+      }
     },
     {
       id: 3,
       nickname: 'what',
       mathscore: 30,
-      email: 'what@gmail.com'
+      email: 'what@gmail.com',
+      homeworks: {
+        done: 20,
+        undone: 180,
+      }
     },
     {
       id: 4,
       nickname: 'jao e o pe de taioba',
       mathscore: 500,
-      email: 'jao@gmail.com'
+      email: 'jao@gmail.com',
+      homeworks: {
+        done: 120,
+        undone: 80,
+      }
     }
 ]
 
