329ea63b829758b1676f685a3da0e71443817a6a
Hugo Ferreira <hugoferreirait@gmail.com>
2022-03-02 22:13:18 -0300

    fix: spaces

--- src/graphql/schemas/UserSchema.ts
+++ src/graphql/schemas/UserSchema.ts
@@ -2,37 +2,37 @@ import { Field, InputType, ObjectType } from "type-graphql";
 
 @InputType()
 export class UserRegisterInput {
-    @Field()
-      name: string
+  @Field()
+    name: string
 
-    @Field()
-      email: string;
+  @Field()
+    email: string;
 
-    @Field()
-      password: string;
+  @Field()
+    password: string;
 }
 
 @InputType()
 export class UserLoginInput {
 
-    @Field()
-      email: string;
+  @Field()
+    email: string;
 
-    @Field()
-      password: string;
+  @Field()
+    password: string;
 }
 
 @ObjectType()
 export class UserAuthResponse {
-    @Field()
-      accessToken: string
+  @Field()
+    accessToken: string
 }
 
 @ObjectType()
 export class UserData {
-    @Field()
-      name: string;
+  @Field()
+    name: string;
 
-    @Field()
-      email: string;
+  @Field()
+    email: string;
 }
