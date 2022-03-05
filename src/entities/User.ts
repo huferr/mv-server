757b2475bd3226ee778e675fa95c356d56687dc4
Hugo Ferreira <hugoferreirait@gmail.com>
2022-03-05 01:59:51 -0300

    chore: cleaning files

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,21 +1,16 @@
-import { Field, Int, ObjectType } from "type-graphql";
 import {Entity, PrimaryGeneratedColumn, Column, BaseEntity} from "typeorm";
-@ObjectType()
+
 @Entity()
 export class User extends BaseEntity {
-    @Field(() => Int)
     @PrimaryGeneratedColumn()
       id: number;
     
-    @Field()
     @Column('text')
       email: string;
 
-    @Field()
     @Column('text')
       password: string;
-      
-    @Field()
+
     @Column('text')
       name: string;
 
