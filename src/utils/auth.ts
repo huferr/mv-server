137bbe0e18a32d1a92c1d2c9d58a01a15a12d43d
Hugo Ferreira <hugoferreirait@gmail.com>
2022-03-06 21:43:03 -0300

    chore: removing token expire date

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -2,9 +2,9 @@ import { sign } from 'jsonwebtoken'
 import { User } from '../entities/User'
 
 export const createAccessToken = (user: User) => {
-  return sign({ userId: user.id }, process.env.ACCESS_TOKEN_SECRET!, { expiresIn: "15m" })
+  return sign({ userId: user.id }, process.env.ACCESS_TOKEN_SECRET!)
 }
 
 export const createRefreshToken = (user: User) => {
-  return sign({ userId: user.id, tokenVersion: user.tokenVersion }, process.env.REFRESH_TOKEN_SECRET!, { expiresIn: "7d" })
+  return sign({ userId: user.id, tokenVersion: user.tokenVersion }, process.env.REFRESH_TOKEN_SECRET!)
 }
